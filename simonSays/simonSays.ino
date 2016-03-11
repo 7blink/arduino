cc4398dc60cfc3224755fdda060a9458f027b81c
7blink <7blinkcreations@gmail.com>
2016-03-11 10:58:06 -0500

    added simon says beginning

--- simonSays/simonSays.ino
+++ simonSays/simonSays.ino
@@ -7,9 +7,12 @@ int input2 = 9;
 int input3 = 10;
 int input4 = 11;
 
-int led1 = 13;
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
 
-int delay = 200;
+int pauseT = 200;
 
 void setup(){
   pinMode(input1, INPUT);
@@ -18,26 +21,38 @@ void setup(){
   pinMode(input4, INPUT);
   
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
   Serial.begin(9600);
   Serial.print("Starting");
   digitalWrite(led1, LOW);
 }
 
+void pause(int inputnum, int lednum){
+  digitalWrite(lednum, HIGH);
+  while(digitalRead(inputnum) == HIGH){
+    delay(pauseT);
+  }
+  digitalWrite(lednum, LOW);
+}
+
 void buttonPressed(){
   if(digitalRead(input1) == HIGH){
     Serial.print("button1");
-    while(digitalRead(input1) == HIGH){
-      delay(delay);
-    }
+    pause(input1, led1);
   }
   if(digitalRead(input2) == HIGH){
     Serial.print("button2");
+    pause(input2, led2);
   }
   if(digitalRead(input3) == HIGH){
     Serial.print("button3");
+    pause(input3, led3);
   }
   if(digitalRead(input4) == HIGH){
     Serial.print("button4");
+    pause(input4, led4);
   }
 }
 void loop(){
